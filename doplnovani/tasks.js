3eda5acf2e4b346bdda654a7debcf4b666fa2a60
mh70cz <mh70@mh70.cz>
2021-02-21 21:22:21 +0100

    tasks

--- doplnovani/tasks.js
+++ doplnovani/tasks.js
@@ -1,10 +1,34 @@
 tasks = [
-  {
+    {
+        north: "vlk",
+        east: "volavka",
+        south: "sova",
+        west: "datel",
+        img: "vlk-volavka-sova-datel.png",
+      },
+
+      {
+        north: "kráva",
+        east: "pes",
+        south: "kočka",
+        west: "labuť",
+        img: "krava-pes-kocka-labut.png",
+      },
+
+    {
+        north: "krokodýl",
+        east: "zebra",
+        south: "delfín",
+        west: "hroch",
+        img: "kroko-zebra-delfin-hroch.png",
+      },
+
+    {
     north: "sever",
     east: "východ",
-    south:"jih",
+    south: "jih",
     west: "západ",
-    img: "svetStrany.png"
+    img: "svetStrany.png",
   },
 
   {
@@ -12,7 +36,7 @@ tasks = [
     east: "podzim",
     south: "zima",
     west: "jaro",
-    img: "rok.png"
+    img: "rok.png",
   },
 
   {
@@ -20,7 +44,14 @@ tasks = [
     east: "voda",
     south: "země",
     west: "vzduch",
-    img: "4-elements-of-nature.jpg"
+    img: "4-elements-of-nature.jpg",
   },
 
+  {
+    north: "kůň",
+    east: "slon",
+    south: "tygr",
+    west: "žirafa",
+    img: "kun-slon-tygr-zirafa.png",
+  },
 ];
