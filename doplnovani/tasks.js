ef226b698c0ef50557e2868426b18c8dbcb11271
mh70cz <mh70@mh70.cz>
2021-02-27 22:53:19 +0100

    tasks

--- doplnovani/tasks.js
+++ doplnovani/tasks.js
@@ -1,4 +1,79 @@
 tasks = [
+
+    {
+        north: "liška",
+        east: "zajíc",
+        south: "jezevec",
+        west: "jelen",
+        img: "liska-zajic-jezevec-jelen.png",
+      },
+    
+    {
+        north: "autobus",
+        east: "kolo",
+        south: "vlak",
+        west: "boty",
+        img: "autobus-kolo-vlak-boty.png",
+      },
+
+    {
+        north: "slunce",
+        east: "měsíc",
+        south: "mrak",
+        west: "blesk",
+        img: "slunce-mesic-mrak-blesk.png",
+      },
+
+    {
+        north: "letadlo",
+        east: "balón",
+        south: "dron",
+        west: "raketa",
+        img: "letadlo-balon-dron-raketa.png",
+      },
+
+      {
+        north: "banán",
+        east: "jablko",
+        south: "citrón",
+        west: "pomeranč",
+        img: "banan-jablko-citron-pomeranc.png",
+      },
+
+      {
+        north: "mrkev",
+        east: "kedluben",
+        south: "okurka",
+        west: "rajče",
+        img: "mrkev-kedluben-okurka-rajce.png",
+      },
+
+    {
+        north: "král",
+        east: "kůň",
+        south: "věž",
+        west: "střelec",
+        img: "kral-kun-vez-strelec.png",
+      },
+
+      {
+        north: "koza",
+        east: "slepice",
+        south: "ovce",
+        west: "kohout",
+        img: "koza-slepice-ovce-kohout.png",
+      },
+
+
+
+      {
+        north: "bagr",
+        east: "buldozer",
+        south: "válec",
+        west: "jeřáb",
+        img: "bagr-buldozer-valec-jerab.png",
+      },
+
     {
         north: "vlk",
         east: "volavka",
