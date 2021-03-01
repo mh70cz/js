3c31c4e6bc43b7f65fbceeef2c46b4bfe8355d76
mh70cz <mh70@mh70.cz>
2021-03-01 18:12:46 +0100

    .

--- doplnovani/tasks.js
+++ doplnovani/tasks.js
@@ -1,6 +1,15 @@
 tasks = [
 
     {
+        north: "sever",
+        east: "východ",
+        south: "jih",
+        west: "západ",
+        img: "svetStrany.png",
+      },
+
+      
+    {
         north: "kůň",
         east: "slon",
         south: "tygr",
@@ -16,117 +25,109 @@ tasks = [
         img: "kroko-zebra-delfin-hroch.png",
       },
 
-//     {
-//         north: "liška",
-//         east: "zajíc",
-//         south: "jezevec",
-//         west: "jelen",
-//         img: "liska-zajic-jezevec-jelen.png",
-//       },
+    {
+        north: "liška",
+        east: "zajíc",
+        south: "jezevec",
+        west: "jelen",
+        img: "liska-zajic-jezevec-jelen.png",
+      },
     
-//     {
-//         north: "autobus",
-//         east: "kolo",
-//         south: "vlak",
-//         west: "boty",
-//         img: "autobus-kolo-vlak-boty.png",
-//       },
+    {
+        north: "autobus",
+        east: "kolo",
+        south: "vlak",
+        west: "boty",
+        img: "autobus-kolo-vlak-boty.png",
+      },
 
-//    {
-//         north: "slunce",
-//         east: "měsíc",
-//         south:  "mrak",
-//         west: "blesk",
-//         img: "slunce-mesic-mrak-blesk.png",
-//       },
+   {
+        north: "slunce",
+        east: "měsíc",
+        south:  "mrak",
+        west: "blesk",
+        img: "slunce-mesic-mrak-blesk.png",
+      },
 
-//     {
-//         north: "letadlo",
-//         east: "balón",
-//         south: "dron",
-//         west: "raketa",
-//         img: "letadlo-balon-dron-raketa.png",
-//       },
+    {
+        north: "letadlo",
+        east: "balón",
+        south: "dron",
+        west: "raketa",
+        img: "letadlo-balon-dron-raketa.png",
+      },
 
-//       {
-//         north: "banán",
-//         east: "jablko",
-//         south: "citrón",
-//         west: "pomeranč",
-//         img: "banan-jablko-citron-pomeranc.png",
-//       },
+      {
+        north: "banán",
+        east: "jablko",
+        south: "citrón",
+        west: "pomeranč",
+        img: "banan-jablko-citron-pomeranc.png",
+      },
 
-//       {
-//         north: "mrkev",
-//         east: "kedluben",
-//         south: "okurka",
-//         west: "rajče",
-//         img: "mrkev-kedluben-okurka-rajce.png",
-//       },
+      {
+        north: "mrkev",
+        east: "kedluben",
+        south: "okurka",
+        west: "rajče",
+        img: "mrkev-kedluben-okurka-rajce.png",
+      },
 
-//     {
-//         north: "král",
-//         east: "kůň",
-//         south: "věž",
-//         west: "střelec",
-//         img: "kral-kun-vez-strelec.png",
-//       },
+    {
+        north: "král",
+        east: "kůň",
+        south: "věž",
+        west: "střelec",
+        img: "kral-kun-vez-strelec.png",
+      },
 
-//       {
-//         north: "koza",
-//         east: "slepice",
-//         south: "ovce",
-//         west: "kohout",
-//         img: "koza-slepice-ovce-kohout.png",
-//       },
+      {
+        north: "koza",
+        east: "slepice",
+        south: "ovce",
+        west: "kohout",
+        img: "koza-slepice-ovce-kohout.png",
+      },
 
-//       {
-//         north: "bagr",
-//         east: "buldozer",
-//         south: "válec",
-//         west: "jeřáb",
-//         img: "bagr-buldozer-valec-jerab.png",
-//       },
+      {
+        north: "bagr",
+        east: "buldozer",
+        south: "válec",
+        west: "jeřáb",
+        img: "bagr-buldozer-valec-jerab.png",
+      },
 
-//     {
-//         north: "vlk",
-//         east: "volavka",
-//         south: "sova",
-//         west: "datel",
-//         img: "vlk-volavka-sova-datel.png",
-//       },
+    {
+        north: "vlk",
+        east: "volavka",
+        south: "sova",
+        west: "datel",
+        img: "vlk-volavka-sova-datel.png",
+      },
 
-//       {
-//         north: "kráva",
-//         east: "pes",
-//         south: "kočka",
-//         west: "labuť",
-//         img: "krava-pes-kocka-labut.png",
-//       },
+      {
+        north: "kráva",
+        east: "pes",
+        south: "kočka",
+        west: "labuť",
+        img: "krava-pes-kocka-labut.png",
+      },
 
-//     {
-//     north: "sever",
-//     east: "východ",
-//     south: "jih",
-//     west: "západ",
-//     img: "svetStrany.png",
-//   },
+  {
+    north: "léto",
+    east: "podzim",
+    south: "zima",
+    west: "jaro",
+    img: "rok.png",
+  },
 
-//   {
-//     north: "léto",
-//     east: "podzim",
-//     south: "zima",
-//     west: "jaro",
-//     img: "rok.png",
-//   },
-
-//   {
-//     north: "oheň",
-//     east: "voda",
-//     south: "země",
-//     west: "vzduch",
-//     img: "4-elements-of-nature.jpg",
-//   },
+  {
+    north: "oheň",
+    east: "voda",
+    south: "země",
+    west: "vzduch",
+    img: "4-elements-of-nature.jpg",
+  },
 
 
 ];
