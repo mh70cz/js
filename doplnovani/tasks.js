d5af00d63154abc758a51e876d33a0ea22bb8835
mh70cz <mh70@mh70.cz>
2021-03-07 21:53:19 +0100

    new tasks

--- doplnovani/tasks.js
+++ doplnovani/tasks.js
@@ -1,103 +1,117 @@
 tasks = [
+  {
+    north: "most",
+    east: "míč",
+    south: "duha",
+    west: "meloun",
+    img: "most-mic-duha-meloun.png",
+  },
 
-    {
-        north: "kladivo",
-        east: "kleště",
-        south: "pila",
-        west: "šroubovák",
-        img: "kladivo-kleste-pila-sroubovak.png",
-      },
+  // kedluben / kedlubna
+  {
+    north: "mrkev",
+    east: "kedluben",
+    south: "okurka",
+    west: "rajče",
+    img: "mrkev-kedluben-okurka-rajce.png",
+  },
 
-    {
-        north: "bagr",
-        east: "buldozer",
-        south: "válec",
-        west: "jeřáb",
-        img: "bagr-buldozer-valec-jerab.png",
-      },
-    
+  //měkčení DI, TĚ
+  {
+    north: "kladivo",
+    east: "kleště",
+    south: "pila",
+    west: "šroubovák",
+    img: "kladivo-kleste-pila-sroubovak.png",
+  },
 
-    {
-        north: "liška",
-        east: "zajíc",
-        south: "jezevec",
-        west: "jelen",
-        img: "liska-zajic-jezevec-jelen.png",
-      },
-    
-    {
-        north: "autobus",
-        east: "kolo",
-        south: "vlak",
-        west: "boty",
-        img: "autobus-kolo-vlak-boty.png",
-      },
+  {
+    north: "bagr",
+    east: "buldozer",
+    south: "válec",
+    west: "jeřáb",
+    img: "bagr-buldozer-valec-jerab.png",
+  },
 
-   {
-        north: "slunce",
-        east: "měsíc",
-        south:  "mrak",
-        west: "blesk",
-        img: "slunce-mesic-mrak-blesk.png",
-      },
+  {
+    north: "lžíce",
+    east: "nůž",
+    south: "talíř",
+    west: "vidlička",
+    img: "lzice-nuz-talir-vidlicka.png",
+  },
 
-    {
-        north: "letadlo",
-        east: "balón",
-        south: "dron",
-        west: "raketa",
-        img: "letadlo-balon-dron-raketa.png",
-      },
+  {
+    north: "liška",
+    east: "zajíc",
+    south: "jezevec",
+    west: "jelen",
+    img: "liska-zajic-jezevec-jelen.png",
+  },
 
-      {
-        north: "banán",
-        east: "jablko",
-        south: "citrón",
-        west: "pomeranč",
-        img: "banan-jablko-citron-pomeranc.png",
-      },
+  {
+    north: "autobus",
+    east: "kolo",
+    south: "vlak",
+    west: "boty",
+    img: "autobus-kolo-vlak-boty.png",
+  },
 
-      {
-        north: "mrkev",
-        east: "kedluben",
-        south: "okurka",
-        west: "rajče",
-        img: "mrkev-kedluben-okurka-rajce.png",
-      },
+  {
+    north: "slunce",
+    east: "měsíc",
+    south: "mrak",
+    west: "blesk",
+    img: "slunce-mesic-mrak-blesk.png",
+  },
 
-    {
-        north: "král",
-        east: "kůň",
-        south: "věž",
-        west: "střelec",
-        img: "kral-kun-vez-strelec.png",
-      },
+  {
+    north: "letadlo",
+    east: "balón",
+    south: "dron",
+    west: "raketa",
+    img: "letadlo-balon-dron-raketa.png",
+  },
 
-      {
-        north: "koza",
-        east: "slepice",
-        south: "ovce",
-        west: "kohout",
-        img: "koza-slepice-ovce-kohout.png",
-      },
+  {
+    north: "banán",
+    east: "jablko",
+    south: "citrón",
+    west: "pomeranč",
+    img: "banan-jablko-citron-pomeranc.png",
+  },
 
+  {
+    north: "král",
+    east: "kůň",
+    south: "věž",
+    west: "střelec",
+    img: "kral-kun-vez-strelec.png",
+  },
 
+  {
+    north: "koza",
+    east: "slepice",
+    south: "ovce",
+    west: "kohout",
+    img: "koza-slepice-ovce-kohout.png",
+  },
 
-    {
-        north: "vlk",
-        east: "volavka",
-        south: "sova",
-        west: "datel",
-        img: "vlk-volavka-sova-datel.png",
-      },
+  {
+    north: "vlk",
+    east: "volavka",
+    south: "sova",
+    west: "datel",
+    img: "vlk-volavka-sova-datel.png",
+  },
 
-      {
-        north: "kráva",
-        east: "pes",
-        south: "kočka",
-        west: "labuť",
-        img: "krava-pes-kocka-labut.png",
-      },
+  {
+    north: "kráva",
+    east: "pes",
+    south: "kočka",
+    west: "labuť",
+    img: "krava-pes-kocka-labut.png",
+  },
 
   {
     north: "léto",
@@ -106,8 +120,8 @@ tasks = [
     west: "jaro",
     img: "rok.png",
   },
- 
-{
+
+  {
     north: "kůň",
     east: "slon",
     south: "tygr",
@@ -138,5 +152,4 @@ tasks = [
     west: "západ",
     img: "svetStrany.png",
   },
-
 ];
